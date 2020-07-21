07b241573538428d9e6f2c2973d0eff0f7b3e35a
solocode <solocode@163.com>
2020-07-22 00:54:31 +0800

    嵌套路由

--- redux-demo02/src/AppRouter.js
+++ redux-demo02/src/AppRouter.js
@@ -1,7 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom"; //引入router
-import Home from './Home';
-import List from './List';
+// import Home from './Home';
+// import List from './List';
+
+import './index.css'
+import Index from './Home'
+//--关键代码------------start
+import Video from './Pages/Video'
+//--关键代码------------end
 /**
  * 两个无状态组件 index & list
  * @param {*} props 
@@ -10,14 +16,35 @@ import List from './List';
 
 function AppRouter() {
     return (
-      <Router>
-          <ul>
-              <li> <Link to="/">首页</Link> </li>
-              <li><Link to="/list/123">列表</Link> </li>
-          </ul>
-          <Route path="/" exact component={Home} />
-          <Route path="/list/:id" component={List} />
-      </Router>
+    //   <Router>
+    //       <ul>
+    //           <li> <Link to="/">首页</Link> </li>
+    //           <li><Link to="/list/123">列表</Link> </li>
+    //       </ul>
+    //       <Route path="/" exact component={Home} />
+    //       <Route path="/list/:id" component={List} />
+    //   </Router>
+        <Router>
+          <div className="mainDiv">
+            <div className="leftNav">
+                <h3>一级导航</h3>
+                <ul>
+                    <li> <Link to="/">博客首页</Link> </li>
+                    {/*--关键代码------------start*/}
+                     <li><Link to="/video/">视频教程</Link> </li>
+                    {/*--关键代码------------end*/}
+                    <li><Link to="">职场技能</Link> </li>
+                </ul>
+            </div>
+
+            <div className="rightMain">
+                <Route path="/"  exact component={Index} />
+                {/*--关键代码------------start*/}
+                <Route path="/video/"   component={Video} />
+                 {/*--关键代码------------end*/}
+            </div>
+          </div>
+        </Router>
     );
 }
 export default AppRouter;
