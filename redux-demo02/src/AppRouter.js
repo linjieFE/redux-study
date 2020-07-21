66ae66a535fc6ae1a66461b6a98b35651cb68cdd
solocode <solocode@163.com>
2020-07-22 01:04:26 +0800

    模拟后台得到JSON数据

--- redux-demo02/src/AppRouter.js
+++ redux-demo02/src/AppRouter.js
@@ -13,6 +13,14 @@ import Workplace from './Pages/workPlace'
 
 
 function AppRouter() {
+    /**
+     * 模拟从后台得到JSON字符串，并转换为了对象（我们只是模拟，就不真的去远端请求数据了）
+     */
+    let routeConfig =[
+        {path:'/',title:'博客首页',exact:true,component:Index},
+        {path:'/video/',title:'视频教程',exact:false,component:Video},
+        {path:'/workplace/',title:'职场技能',exact:false,component:Workplace}
+    ]
     return (
     //   <Router>
     //       <ul>
@@ -27,20 +35,31 @@ function AppRouter() {
             <div className="leftNav">
                 <h3>一级导航</h3>
                 <ul>
-                    <li> <Link to="/">博客首页</Link> </li>
+                    {
+                        routeConfig.map((item,index)=>{
+                            return (<li key={index}> <Link to={item.path}>{item.title}</Link> </li>)
+                        })
+                    }
+                    {/* <li> <Link to="/">博客首页</Link> </li> */}
                     {/*--关键代码------------start*/}
-                     <li><Link to="/video/">视频教程</Link> </li>
+                     {/* <li><Link to="/video/">视频教程</Link> </li> */}
                     {/*--关键代码------------end*/}
-                    <li><Link to="/workplace/">职场技能</Link> </li>
+                    {/* <li><Link to="/workplace/">职场技能</Link> </li> */}
                 </ul>
             </div>
 
             <div className="rightMain">
-                <Route path="/"  exact component={Index} />
+                {
+                    routeConfig.map((item,index)=>{
+                        return (<Route key={index} exact={item.exact} path={item.path}  component={item.component} />)
+                    })
+                }
+
+                {/* <Route path="/"  exact component={Index} /> */}
                 {/*--关键代码------------start*/}
-                <Route path="/video/"   component={Video} />
+                {/* <Route path="/video/"   component={Video} /> */}
                  {/*--关键代码------------end*/}
-                <Route path="/workplace/"   component={Workplace} />
+                {/* <Route path="/workplace/"   component={Workplace} /> */}
             </div>
           </div>
         </Router>
